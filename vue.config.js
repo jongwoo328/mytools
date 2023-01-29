77fec15ef49a15b3aa62071dd00d6c73549b3b75
JongwooLee <jongwoo328@gmail.com>
2023-01-29 22:59:43 +0900

    add icon, update title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: "My Tools",
+    },
+  },
 });
