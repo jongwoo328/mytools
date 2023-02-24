83f1a37cd38a6e17b7c19e1db8916f53e2ab725d
이종우 (Lee Jongwoo) <jongwoo328@gmail.com>
2023-02-25 00:04:29 +0900

    gtag 라이브러리 설정 추가 (#38)

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "vue-json-pretty/lib/styles.css";
 
+import VueGtag from "vue-gtag-next";
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import AOS from "aos";
@@ -24,5 +26,16 @@ createApp(App)
   .use(VueCodemirror, {
     disabled: true,
     extensions: [],
+  })
+  .use(VueGtag, {
+    appName: "My Tools",
+    pageTrackerScreenviewEnabled: true,
+    config: {
+      id: "G-5LVPSWJ2CB",
+      params: {
+        send_page_view: false,
+      },
+    },
+    router,
   })
   .mount("#app");
