7a7d49443ac710220da3e06371fd78c626700184
Taein Kim <kti91@likelion.org>
2023-03-18 18:08:13 +0900

    fix: 타입에러 보정 (#41)
    * fix: 타입에러 보정
    
    * router tracking 추가
    
    ---------
    
    Co-authored-by: JongwooLee <jongwoo328@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,14 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "vue-json-pretty/lib/styles.css";
 
-import VueGtag from "vue-gtag-next";
+import VueGtag, { trackRouter } from "vue-gtag-next";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import AOS from "aos";
 import "aos/dist/aos.css";
+
+trackRouter(router, { useScreenview: true, skipSamePath: true });
 
 AOS.init();
 createApp(App)
@@ -29,13 +31,11 @@ createApp(App)
   })
   .use(VueGtag, {
     appName: "My Tools",
-    pageTrackerScreenviewEnabled: true,
-    config: {
+    property: {
       id: "G-5LVPSWJ2CB",
       params: {
         send_page_view: false,
       },
     },
-    router,
   })
   .mount("#app");
