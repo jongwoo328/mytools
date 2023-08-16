af30fcd10a5b9cde6cd52420dc2e3dcbc5891abb
JongwooLee <jongwoo328@gmail.com>
2023-08-16 15:47:41 +0900

    change image url

--- pages/converter/image-crop.vue
+++ pages/converter/image-crop.vue
@@ -11,7 +11,7 @@ const imageInput = ref() as Ref<HTMLInputElement>;
 const cropper = ref();
 const cropperImageSource = ref(
   // eslint-disable-next-line max-len
-  "https://images.unsplash.com/photo-1636622433525-127afdf3662d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=100",
+  "https://img.jongwoo.me/tools/ghost.avif",
 );
 
 const selectedAspectRatio: Ref<"Custom" | number> = ref(16 / 9);
