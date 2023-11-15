684631ae5cc102b38b10cfaf6202664bd29001e5
JongwooLee <jongwoo328@gmail.com>
2023-11-15 23:15:55 +0900

    add image to public folder, change cropper image src

--- pages/converter/image-crop.vue
+++ pages/converter/image-crop.vue
@@ -28,7 +28,6 @@ const { asyncBlobToBase64 } = useImageUtil();
 const image = ref() as Ref<HTMLImageElement>;
 const imageInput = ref() as Ref<HTMLInputElement>;
 const cropper = ref();
-const cropperImageSource = ref("https://s3.ap-northeast-2.amazonaws.com/public-store.jongwoo.me/tools/ghost.png");
 
 const selectedAspectRatio: Ref<"Custom" | number> = ref(16 / 9);
 const customRatioWidth = ref(1.618);
@@ -292,7 +291,7 @@ watch([pressUp, pressRight, pressLeft, pressDown], () => {
               :check-orientation="false"
               :check-cross-origin="false"
               :aspect-ratio="selectedAspectRatio"
-              :src="cropperImageSource"
+              src="/static/images/ghost.png"
               @ready="onCropperReady"
               @cropmove="onCropResize"
               :style="{ height: '100%', width: '100%' }"
