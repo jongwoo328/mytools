e4b756e2d05d965aef3756783f3b1057e7530b62
JongwooLee <jongwoo328@gmail.com>
2024-01-20 00:20:20 +0900

    change i18n strategy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   // nuxtjs/i18n
   i18n: {
     vueI18n: "./i18n.config.ts",
-    strategy: "prefix",
+    strategy: "prefix_and_default",
     defaultLocale: "en",
     locales: [
       {
