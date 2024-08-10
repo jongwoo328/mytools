c6f8d89534a02d3882e84bd428aca2c38a9e28d9
JongwooLee <jongwoo328@gmail.com>
2024-08-10 20:30:03 +0900

    change primary color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import eslintPlugin from "@nabla/vite-plugin-eslint";
 import fs from "fs";
 import Lara from "@primevue/themes/lara";
 import defaultOptions from "@primevue/core/config";
+import { definePreset } from "@primeuix/styled";
 
 const siteUrl = "https://tools.jongwoo.me";
 
@@ -41,7 +42,23 @@ export default defineNuxtConfig({
         noFileChosenMessage: "",
       },
       theme: {
-        preset: Lara,
+        preset: definePreset(Lara, {
+          semantic: {
+            primary: {
+              50: "{blue.50}",
+              100: "{blue.100}",
+              200: "{blue.200}",
+              300: "{blue.300}",
+              400: "{blue.400}",
+              500: "{blue.500}",
+              600: "{blue.600}",
+              700: "{blue.700}",
+              800: "{blue.800}",
+              900: "{blue.900}",
+              950: "{blue.950}",
+            },
+          },
+        }),
       },
     },
   },
